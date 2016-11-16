e9eea598ca0137951011a3e01d31437cfb36d0c1
AlexZeem <alexzeem@gmail.com>
2016-11-16 13:15:42 +0200

    Lesson 2: draw square, circle, triangle (UPD)

--- l2_use_classes/mindstorms.py
+++ l2_use_classes/mindstorms.py
@@ -1,36 +1,41 @@
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
+    for i in range(1, 5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_triangle(some_turtle):
+    some_turtle.forward(100)
+    some_turtle.left(120)
+    some_turtle.forward(100)
+    some_turtle.left(120)
+    some_turtle.forward(100)
+
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    #Create the turtle Brad - Draws a square
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("yellow")
     brad.speed(2)
-    
-    for i in range(0, 4):
-        brad.forward(100)
-        brad.right(90)
+    draw_square(brad)
 
-def draw_circle():
+    #Create the turtle Angie - Draws a circle
     angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("blue")
     angie.circle(100)
 
-def draw_triangle():
-    mary = turtle.Turtle()
-    mary.shape("circle")
-    mary.color("green")
+    #Create the turtle Tom - Draws a triangle
+    tom = turtle.Turtle()
+    tom.shape("circle")
+    tom.color("green")
+    draw_triangle(tom)
+    
+    window.exitonclick()
 
-    #for i in range(0, 1):
-    mary.forward(100)
-    mary.left(120)
-    mary.forward(100)
-    mary.left(120)
-    mary.forward(100)
-        
-window = turtle.Screen()
-window.bgcolor("red")
-draw_square()
-draw_circle()
-draw_triangle()
-window.exitonclick()
+draw_art()
+
