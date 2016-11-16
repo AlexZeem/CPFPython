f2e4f5460f6ee7567163249a8727041c1168cc56
AlexZeem <alexzeem@gmail.com>
2016-11-16 17:41:09 +0200

    Create and draw flover

--- l2_use_classes/Turtle/flower.py
+++ l2_use_classes/Turtle/flower.py
@@ -1,8 +1,10 @@
 import turtle
 
-def create(color):
-    print("Create a flower")
-
-def draw(turtle):
-    print("Draw a flower")
-
+def draw(some_turtle, size):
+    for _ in range(1, 21):
+        some_turtle.forward(size)
+        some_turtle.left(162)
+    some_turtle.forward(size / 2)
+    some_turtle.right(90)
+    some_turtle.forward(size)
+    
--- l2_use_classes/Turtle/main.py
+++ l2_use_classes/Turtle/main.py
@@ -6,17 +6,34 @@ import initials
 def draw_art():
     window = turtle.Screen()
     window.bgcolor("white")
-
-    my_flower = flower.create("blue")
-    flower.draw(my_flower)
-
-    my_fractal = fractal.create("green")
-    fractal.draw(my_fractal)
-
-    my_initials = initials.create("red")
-    initials.draw(my_initials)
+    
+    #Create and draw flover
+    my_flower = turtle.Turtle()
+    my_flower.shape("turtle")
+    my_flower.color("blue")
+    my_flower.speed(2)
+    my_flower.penup()
+    my_flower.goto(-300, 0)
+    my_flower.pendown()
+    flower.draw(my_flower, 200)
     
     window.exitonclick()
+'''
+    #Create and draw fractal
+    my_fractal = turtle.Turtle()
+    my_fractal.shape("turtle")
+    my_fractal.color("green")
+    my_fractal.speed(3)
+    fractal.draw(my_fractal)
+
+    #Create and draw initials
+    my_initials = turtle.Turtle()
+    my_initials.shape("turtle")
+    my_initials.color("green")
+    my_initials.speed(4)
+    initials.draw(my_initials)
+'''
+    
 
 draw_art()
 
