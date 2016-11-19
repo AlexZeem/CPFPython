05d0f6be3e113f4861199f37b6d9305906e86a61
AlexZeem <alexzeem@gmail.com>
2016-11-19 10:04:45 +0200

    Create and draw fractal: base triangle

--- l2_use_classes/Turtle/main.py
+++ l2_use_classes/Turtle/main.py
@@ -8,6 +8,52 @@ def draw_flover(some_turtle, size):
     some_turtle.right(90)
     some_turtle.forward(size)
 
+def draw_fractal(some_turtle, size, color):
+    def __draw_triangle(some_turtle, size):
+        for _ in range(1, 4):
+            some_turtle.forward(size)
+            some_turtle.left(120)
+
+    some_turtle.begin_fill()
+    some_turtle.fillcolor(color)
+
+    for _ in range(1, 9):
+        __draw_triangle(some_turtle, size / 8)
+        some_turtle.forward(size / 8)
+
+    some_turtle.left(120)
+    some_turtle.forward(size / 8)
+
+    some_turtle.tilt(120)
+    for _ in range(1, 8):
+        __draw_triangle(some_turtle, size / 8)
+        some_turtle.forward(size / 8)
+
+    some_turtle.left(120)
+    some_turtle.forward(size / 8)
+
+    for _ in range(1, 7):
+        __draw_triangle(some_turtle, size / 8)
+        some_turtle.forward(size / 8)
+    '''
+    some_turtle.left(120)
+    some_turtle.forward(size / 8)
+
+    some_turtle.left(120) 
+    some_turtle.forward(size / 8)
+
+    some_turtle.right(120) 
+    some_turtle.forward(size / 4)
+
+    some_turtle.left(120) 
+    some_turtle.forward(size / 8)
+
+    some_turtle.left(120) 
+    some_turtle.forward(size / 8)
+    '''
+    some_turtle.end_fill()
+    
+
 def draw_art():
     window = turtle.Screen()
     window.bgcolor("white")
@@ -16,21 +62,22 @@ def draw_art():
     flover = turtle.Turtle()
     flover.shape("turtle")
     flover.color("blue")
-    flover.speed(2)
     flover.penup()
     flover.goto(-300, 0)
     flover.pendown()
-    draw_flover(flover, 200)
+    #draw_flover(flover, 200)
     
+    #Create and draw fractal
+    fractal = turtle.Turtle()
+    fractal.shape("turtle")
+    fractal.color("green")
+    fractal.penup()
+    fractal.goto(-100, -50)
+    fractal.pendown()
+    draw_fractal(fractal, 200, "green")
+
     window.exitonclick()
 '''
-    #Create and draw fractal
-    my_fractal = turtle.Turtle()
-    my_fractal.shape("turtle")
-    my_fractal.color("green")
-    my_fractal.speed(3)
-    fractal.draw(my_fractal)
-
     #Create and draw initials
     my_initials = turtle.Turtle()
     my_initials.shape("turtle")
